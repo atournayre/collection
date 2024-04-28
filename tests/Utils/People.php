03d6ee286938881bccd3334acc4aeeff68709643
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2024-04-28 14:03:26 +0200

    Add validateElement method to AbstractCollection to help add specific validation to each element

--- tests/Utils/People.php
+++ tests/Utils/People.php
@@ -2,9 +2,15 @@
 
 namespace Atournayre\Collection\Tests\Utils;
 
+use Atournayre\Assert\Assert;
 use Atournayre\Collection\TypedCollection;
 
 class People extends TypedCollection
 {
     protected static string $type = Person::class;
+
+    protected function validateElement(mixed $value): void
+    {
+        Assert::lengthBetween($value->name, 1, 30);
+    }
 }
